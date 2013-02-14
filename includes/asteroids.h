331f5b9f28c616f6612a3b0abe2c34fc6ec99098
k3rb3ros <k3rb3ros@Nosferatu.(none)>
2013-02-14 15:22:32 -0700

    Set up .so's in linux folder set up pre-processor O.S detection in asteroids.h

---
+++ includes/asteroids.h
@@ -0,0 +1,10 @@
+#ifdef __linux__
+	//Linux includes go here (assumed to be 64 bit linux)
+
+#elif defined _WIN32
+	//32 bit Windows includes go here
+
+#elif defined _WIN64
+	//64 bit Windows includes go here
+
+#endif
