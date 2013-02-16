7d6681d0714be68adb2a0d0979f4963b36c0ef7b
k3rb3ros <k3rb3ros@Nosferatu.(none)>
2013-02-15 18:08:46 -0700

    Added Frame and Animation classes (need to be debugged)

--- includes/asteroids.h
+++ includes/asteroids.h
@@ -1,10 +1,21 @@
-#ifdef __linux__
-	//Linux includes go here (assumed to be 64 bit linux)
+#ifndef ASTEROIDS_H  //double include protection
+#define ASTEROIDS_H
 
-#elif defined _WIN32
-	//32 bit Windows includes go here
+	#ifdef __linux__
+		//Linux includes go here (assumed to be 64 bit linux)
+		#include "SDL.h"
+		#include <climits>
+		#include <iostream>
+		#include <fstream>
+		#include <string>
+		#include <vector>
 
-#elif defined _WIN64
-	//64 bit Windows includes go here
+	#elif defined _WIN32
+		//32 bit Windows includes go here
+		#include "SDL.h"
 
+	#elif defined _WIN64
+		//64 bit Windows includes go here
+		#include "SDL.h"
+	#endif
 #endif
---
+++ src/game.cpp
@@ -0,0 +1,71 @@
+#include "../includes/asteroids.h"
+class Frame
+{
+	SDL_Surface *image2;
+	SDL_Rect src,dest;
+	int time;
+	public:
+	long getTime() { return time; }
+	void init (string fname,SDL_Surface *screen,long newTime=LONG_MAX /*ms*/)
+	{
+		time = newTime;
+		SDL_Surface *image;
+		int colorKey;
+		image = SDL_LoadBMP(fname.c_str());
+		image2 = SDL_MapRGB(screen -> format, 0,0,255);
+		colorKey = SDL_ConvertSurface(image,screen -> format, SDL_HWSURFACE);
+		SDL_SetColorKey(image2, SDL_SRCCOLORKEY,colorKey);
+		SDL_FreeSurface(image);
+		src.x=0; src.y=0; src.w=image2->w; src.h=image2 -> h;
+		dest.x=0; dest.y=0; dest.w = image2 -> w; dest.h = image2 -> h;
+	}
+	void draw(SDL_Surface *screen,int x, int y)
+	{
+		dest.x=x; dest.y=y;
+		SDL_BlitSurface(image2,&src,screen,&dest);
+	}
+};
+
+class Animation
+{
+	vector<Frame> frames;
+	long totalTime;
+	public:
+	void init(String fname,SDL_Surface *screen)
+	{
+		int n;
+		ifstream in(fname.c_str());
+		in >> in;
+		totalTime=0;
+		for(int i=0; i<n; i++)
+		{
+			long t;
+			string f2name;
+			in >> t >> f2name;
+			totalTime+=t;
+			Frame f;
+			f.init(f2name,screen,t);
+			frames.insert(frames.end(),f;)
+		}
+		in.close();
+	}
+	void draw(SDL_Surface *screen,int x, int y, long elapsed /*ms*/)
+	{
+		long currentFrameTime=elapsed % totalTime;
+		for(int i=1; i<frames.size(); i++)
+		{
+			if(frames[i].getTime() > currentFrameTime)
+			{
+				frames[i].draw(screen,x,y);
+				break;
+			}
+			currentFrameTime-=frames[i].getTime();
+		}
+	}
+};
+using namespace std;
+int main(int argc, char** argv)
+{
+	cout << "Hurpa Derp" << endl;
+	return 0;
+}
