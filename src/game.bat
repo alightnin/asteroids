5ce3742912a1f317739e788926701d798b9540d5
Craig Weir <alightnin@gmail.com>
2013-03-17 14:46:31 -0600

    change .bat for updated code and added a file to gitignore

--- src/game.bat
+++ src/game.bat
@@ -1,2 +1,2 @@
-g++ -w -O0 -g -Wall -Wextra game.cpp set_up.cpp mixer.cpp CSurface.cpp -o ../bin/asteroids.exe -I ../include -L ../bin -lmingw32 -lSDLmain -lSDL -lSDL_image -lSDL_mixer -lSDL_gfx
+g++ -w -O0 -g -Wall -Wextra game.cpp game_events.cpp ship.cpp mixer.cpp CSurface.cpp -o ../bin/asteroids.exe -I ../include -L ../bin -lmingw32 -lSDLmain -lSDL -lSDL_image -lSDL_mixer -lSDL_gfx
 pause
